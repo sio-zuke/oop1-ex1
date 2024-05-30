81b12ab58112ba6a1f583ff2c86115005060e2b3
siozuke <siozuke0004@gmail.com>
2024-05-30 14:36:33 +0900

    1-4の実施

--- src/main/java/Ex1.java
+++ src/main/java/Ex1.java
@@ -36,7 +36,32 @@ public class Ex1 {
 
     // 1-4
     public static String num2str (int n) {
-        return "zero";
+        switch (n) {
+            case 0:
+                return "zero";
+            case 1:
+                return "one";
+            case 2:
+                return "two";
+            case 3:
+                return "three";
+            case 4:
+                return "four";
+            case 5:
+                return "five";
+            case 6:
+                return "six";
+            case 7:
+                return "seven";
+            case 8:
+                return "eignt";
+            case 9:
+                return "nine";
+            case 10:
+                return "ten";
+            default:
+                return "error"; 
+        }
     }
 
     // 1-5
